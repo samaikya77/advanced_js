3de5f9b6e7a8d4f67cd0affa189bdd066247be2c
samaikya77 <sami.venna@gmail.com>
2025-02-07 18:13:21 +0100

    Add files via upload

---
+++ promise.js
@@ -0,0 +1,28 @@
+/*var mypromise=new Promise((resolve,reject)=>{
+
+  var isright=true
+  if(isright==true){
+    resolve('she is right')
+  }else{
+    reject('she is wrong')
+  }
+})
+mypromise
+.then((result)=>{
+    console.log(result);
+})
+.catch((result)=>{
+    console.log(result);
+})
+.finally(()=>{
+    console.log('code executed')
+})*/
+
+
+try{
+    var a=jskjsd
+    console.log(a);
+}
+catch(error){
+    console.log( 'a wiil show error');
+}
