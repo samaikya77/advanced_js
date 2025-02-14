9b5e179cdead9502e1a9b6c8ff8132653b1481e8
Samaikya Venna <sam@Samaikyas-MacBook-Air.local>
2025-02-14 20:35:14 +0100

    strings,function

---
+++ script.js
@@ -0,0 +1,22 @@
+/*function Check(marks)
+    if(marks<=35){
+        console.log('fail');
+    }else if(marks>35){
+        console.log('pass');
+    }else {
+        console.log('enter a number')
+    }
+}
+Check(45)*/
+
+
+var a=20
+var b=22
+var c=25
+if(a>b && a>c){
+    console.log('a is bigger')
+}else if(b>a && b>c){
+    console.log('b is bigger')
+}else{
+    console.log('c is bigger')
+}
---
+++ string.js
@@ -0,0 +1,57 @@
+var str='java script'
+console.log(str.toString());
+
+var str='java script'
+console.log(str.charAt(4));
+
+var str='JAVA SCRIPT'
+console.log(str.toLowerCase());
+
+var str='java script'
+console.log(str.toUpperCase())
+
+var str='java script'
+console.log(str.startsWith('java'));
+
+var str='java script'
+console.log(str.endsWith('t'));
+
+var str='java'
+console.log(str.padEnd(5,'sam'));
+
+var str10 = "sai"
+console.log(str10.padEnd(4,"java"));
+
+var str='sam'
+console.log(str.padStart('java',1));
+
+var str='java script'
+console.log(str.slice(2,6))
+
+var Array=[1,2,3,4,5]
+console.log(Array.splice(0,3));
+
+var str='java script'
+console.log(str.substring(1,7))
+
+var str='coders'
+console.log(str.replace('coders', 'c'));
+
+var str='samaikya'
+console.log(str.split(' ,'))
+
+var str='   samaikya   '
+console.log(str.trim());
+
+var str='samaikya'
+console.log(str.split('').reverse().join(''))
+
+var str='sam'
+console.log(str.length);
+
+var str='sam'
+console.log(str.repeat(2));
+
+var str1='sam'
+var str2='satya'
+console.log(str1.concat(str2));
